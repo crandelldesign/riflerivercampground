9b20a8abb982fd84c7008d9bb6121e8d464418fc
Crandell, Matt <mcrandell@jrthompson.com>
2016-03-15 09:26:33 -0400

    Updated gulp file and began adding dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,37 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    // Build Stylesheet
+    mix.sass(
+        'stylesheet.scss',
+        'public/css/stylesheet.css',
+        {
+            includePaths: [
+                'node_modules/bootstrap-sass/assets/stylesheets/',
+                'node_modules/font-awesome/scss/'
+            ]
+        }
+    );
+    // Copy Bootstrap's JS
+    mix.copy('node_modules/bootstrap-sass/assets/javascripts/*.js', 'resources/assets/js/bootstrap');
+    // Build JS
+    mix.scripts(
+        [
+            'jquery-2.2.1.js',
+            'bootstrap/bootstrap.js',
+            'master.js'
+        ],
+        'public/js/master.js'
+    );
+    // Copy Fonts
+    mix.copy('node_modules/bootstrap-sass/assets/fonts', 'public/fonts/bootstrap');
+    mix.copy('node_modules/font-awesome/fonts', 'public/fonts');
+
+    // Create Build Numbers
+    mix.version(['css/stylesheet.css', 'js/master.js']);
+
+    // Copy Fonts and Images for Build Numbers
+    mix.copy('node_modules/bootstrap-sass/assets/fonts', 'public/build/fonts/bootstrap');
+    mix.copy('node_modules/font-awesome/fonts', 'public/build/fonts');
+    mix.copy('public/img', 'public/build/img');
 });
