4e3a30fa8f1a073d7fa4c19200525f4f50ace4a5
Matt Crandell <mrcrandell@gmail.com>
2016-04-19 18:41:50 -0400

    Added new photos and photo gallery. Updated styling.

--- resources/views/home/photos.blade.php
+++ resources/views/home/photos.blade.php
@@ -8,13 +8,43 @@
 
 <div class="row">
     <div class="col-sm-6 col-md-3">
-        <a class="img-modal" data-caption="Our youngest guests enjoy hours of fun on our Rainbow Play System." href="{{url('/img/photos/playground2.jpg')}}"><img class="img-responsive" alt="Our youngest guests enjoy hours of fun on our Rainbow Play System." src="{{url('/img/photos/playground2.jpg')}}"></a>
+        <a class="img-modal" data-caption="Our youngest guests enjoy hours of fun on our Rainbow Play System." href="{{url('/img/photos/playground2.jpg')}}"><img class="img-responsive" alt="Our youngest guests enjoy hours of fun on our Rainbow Play System." src="{{url('/img/photos/playground2-small.jpg')}}"></a>
     </div>
     <div class="col-sm-6 col-md-3">
-        <a class="img-modal" data-caption="Peaceful scenes abound on our hiking trail." href="{{url('/img/photos/hiking2.jpg')}}"><img class="img-responsive" alt="Peaceful scenes abound on our hiking trail." src="{{url('/img/photos/hiking2.jpg')}}"></a>
+        <a class="img-modal" data-caption="Peaceful scenes abound on our hiking trail." href="{{url('/img/photos/hiking2.jpg')}}"><img class="img-responsive" alt="Peaceful scenes abound on our hiking trail." src="{{url('/img/photos/hiking2-small.jpg')}}"></a>
     </div>
     <div class="col-sm-6 col-md-3">
-        <a class="img-modal" href="{{url('/img/photos/hiking1.jpg')}}"><img class="img-responsive" alt="Hiking" src="{{url('/img/photos/hiking1.jpg')}}"></a>
+        <a class="img-modal" href="{{url('/img/photos/hiking1.jpg')}}"><img class="img-responsive" alt="Hiking" src="{{url('/img/photos/hiking1-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/fawn.jpg')}}"><img class="img-responsive" alt="Fawn" src="{{url('/img/photos/fawn-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/eagle2.jpg')}}"><img class="img-responsive" alt="Eagle" src="{{url('/img/photos/eagle2-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo1.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 1" src="{{url('/img/photos/photo1-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo2.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 2" src="{{url('/img/photos/photo2-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo3.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 3" src="{{url('/img/photos/photo3-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo4.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 4" src="{{url('/img/photos/photo4-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo5.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 5" src="{{url('/img/photos/photo5-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo6.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 6" src="{{url('/img/photos/photo6-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo7.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 7" src="{{url('/img/photos/photo7-small.jpg')}}"></a>
+    </div>
+    <div class="col-sm-6 col-md-3">
+        <a class="img-modal" href="{{url('/img/photos/photo8.jpg')}}"><img class="img-responsive" alt="Photo of Rifle River Campground 8" src="{{url('/img/photos/photo8-small.jpg')}}"></a>
     </div>
 </div>
 
