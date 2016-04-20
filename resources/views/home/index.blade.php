4f15d9c2cebfa5788ba93c122171cebaf10c1e86
Crandell, Matt <mcrandell@jrthompson.com>
2016-04-20 08:55:48 -0400

    Updated links to ensure everything is correct and formatted properly.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,9 +4,9 @@
 <div class="subhead">
     <h1>Peaceful Camping and Canoeing in beautiful Sterling, MI</h1>
     <div>
-        <a href="{{url('/')}}/park-map" class="btn btn-blue btn-lg">See Our Campground</a>
+        <a href="{{url('/park-map')}}" class="btn btn-blue btn-lg">See Our Campground</a>
         <div class="clearfix visible-xs-block"></div>
-        <a href="{{url('/')}}/contact" class="btn btn-green btn-lg">Contact Us</a>
+        <a href="{{url('/contact')}}" class="btn btn-green btn-lg">Contact Us</a>
     </div>
 </div>
 @stop
--- resources/views/master/templates/nav.blade.php
+++ resources/views/master/templates/nav.blade.php
@@ -20,17 +20,17 @@
                 <ul class="nav navbar-nav">
                     <li class="{{(isset($active_page)) && $active_page=='home'?'active':''}}"><a title="Home" href="{{url('/')}}">Home</a></li>
                     <li class="{{(isset($active_page)) && $active_page=='camping'?'active':''}}"><a title="Camping" href="{{url('/camping')}}">Camping</a></li>
-                    <li class="{{(isset($active_page)) && $active_page=='cabins'?'active':''}}"><a title="Cabins" href="{{url('/')}}/cabins">Cabins</a></li>
+                    <li class="{{(isset($active_page)) && $active_page=='cabins'?'active':''}}"><a title="Cabins" href="{{url('/cabins')}}">Cabins</a></li>
                     <li class="{{(isset($active_page)) && $active_page=='river-trips'?'active':''}} dropdown"><a title="River Trips" href="#" data-toggle="dropdown" class="dropdown-toggle" aria-haspopup="true">River Trips <span class="caret"></span></a>
                         <ul role="menu" class=" dropdown-menu">
-                            <li><a title="Canoeing" href="{{url('/')}}/river-trips/canoeing/">Canoeing</a></li>
-                            <li><a title="Kayaking" href="{{url('/')}}/river-trips/kayaking/">Kayaking</a></li>
-                            <li><a title="Tubing" href="{{url('/')}}/river-trips/tubing/">Tubing</a></li>
-                            <li><a title="Specials" href="{{url('/')}}/river-trips/specials/">Specials</a></li>
+                            <li><a title="Canoeing" href="{{url('/river-trips/canoeing')}}">Canoeing</a></li>
+                            <li><a title="Kayaking" href="{{url('/river-trips/kayaking')}}">Kayaking</a></li>
+                            <li><a title="Tubing" href="{{url('/river-trips/tubing')}}">Tubing</a></li>
+                            <li><a title="Specials" href="{{url('/river-trips/specials')}}">Specials</a></li>
                         </ul>
                     </li>
-                    <li class="{{(isset($active_page)) && $active_page=='park-map'?'active':''}}"><a title="Park Map" href="{{url('/')}}/park-map">Park Map</a></li>
-                    <li class="{{(isset($active_page)) && $active_page=='photos'?'active':''}}"><a title="Photos" href="{{url('/')}}/photos">Photos</a></li>
+                    <li class="{{(isset($active_page)) && $active_page=='park-map'?'active':''}}"><a title="Park Map" href="{{url('/park-map')}}">Park Map</a></li>
+                    <li class="{{(isset($active_page)) && $active_page=='photos'?'active':''}}"><a title="Photos" href="{{url('/photos')}}">Photos</a></li>
                     <li class="{{(isset($active_page)) && $active_page=='contact'?'active':''}}"><a title="Contact" href="{{url('/contact')}}">Contact</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
