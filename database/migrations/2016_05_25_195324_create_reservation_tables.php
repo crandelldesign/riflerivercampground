1219923d3561904fb00e585ae60e7c5f7de32b2f
Crandell, Matt <mcrandell@jrthompson.com>
2016-05-26 16:04:05 -0400

    Updated database. Migration required.

--- database/migrations/2016_05_25_195324_create_reservation_tables.php
+++ database/migrations/2016_05_25_195324_create_reservation_tables.php
@@ -16,6 +16,40 @@ class CreateReservationTables extends Migration
             $table->timestamps();
             $table->increments('id');
             $table->integer('site_id');
+            $table->string('type');
+            $table->float('adult_price');
+            $table->float('child_price');
+        });
+
+        Schema::create('cabin_sites', function (Blueprint $table) {
+            $table->timestamps();
+            $table->increments('id');
+            $table->integer('site_id');
+            $table->float('price');
+            $table->float('additional_adult_price');
+            $table->float('additional_child_price');
+            $table->integer('max_capacity')->default(6);
+        });
+
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->timestamps();
+            $table->increments('id');
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+            $table->integer('site_id');
+            $table->integer('reservationable_id');
+            $table->string('reservationable_type');
+            $table->integer('adult_count');
+            $table->integer('children_count');
+            $table->float('price');
+        });
+
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->timestamps();
+            $table->increments('id');
+            $table->dateTime('starts_at');
+            $table->dateTime('ends_at');
+            $table->string('title');
         });
     }
 
@@ -27,5 +61,8 @@ class CreateReservationTables extends Migration
     public function down()
     {
         Schema::drop('camp_sites');
+        Schema::drop('cabin_sites');
+        Schema::drop('reservations');
+        Schema::drop('holidays');
     }
 }
