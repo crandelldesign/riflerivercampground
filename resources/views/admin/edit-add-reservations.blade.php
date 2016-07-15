4ce14a7d3bada2ea923dcb4ddd6e3747fb501c75
Crandell, Matt <mcrandell@jrthompson.com>
2016-07-15 16:02:53 -0400

    Updated reservations admin page.

--- resources/views/admin/edit-add-reservations.blade.php
+++ resources/views/admin/edit-add-reservations.blade.php
@@ -1,22 +1,37 @@
 @extends('layouts.admin')
 @section('content-header')
-    <h1>{{(isset($reservation))?'Edit':'Add'}} Reservation</h1>
+    <h1>{{(isset($reservation))?'Reservation #'.$reservation->id:'Add Reservation'}}</h1>
 @stop
 @section('content')
 
+@if (isset($reservation))
 <div class="box">
+    <div class="box-body">
+
+        <div class="text-center">
+            <button class="btn btn-lg btn-primary">Check In</button>
+            <button class="btn btn-lg btn-success">Approve/Deny</button>
+        </div>
+
+    </div>
+</div>
+@endif
+
+<div class="box">
+
     <div class="box-header with-border">
         <h2 class="box-title">{{(isset($reservation))?'Edit':'Add'}} Reservation</h2>
     </div>
     <div class="box-body">
 
         <form action="{{url('/reservations')}}" method="post">
+            <p class="text-right"><button class="btn btn-primary">Save Changes</button></p>
             <div class="row">
                 <div class="col-sm-6 col-md-4">
                     <div class="form-group">
                         <label>From</label>
                         <div class="input-group date" id="starts_at">
-                            <input class="form-control date" name="starts_at" type="text" placeholder="{{date('m/d/Y')}}" value="{{old('starts_at')?old('starts_at'):isset($starts_at)?date('m/d/Y',$starts_at):''}}">
+                            <input class="form-control date" name="starts_at" type="text" placeholder="{{date('m/d/Y')}}" value="{{(isset($reservation))?date('m/d/Y',strtotime($reservation->starts_at)):''}}">
                             <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
                         </div>
                     </div>
@@ -25,7 +40,7 @@
                     <div class="form-group">
                         <label>To</label>
                         <div class="input-group date" id="ends_at">
-                            <input class="form-control date" name="ends_at" type="text" placeholder="{{date('m/d/Y',strtotime('+3 days'))}}" value="{{old('ends_at')?old('ends_at'):isset($ends_at)?date('m/d/Y',$ends_at):''}}">
+                            <input class="form-control date" name="ends_at" type="text" placeholder="{{date('m/d/Y',strtotime('+3 days'))}}" value="{{(isset($reservation))?date('m/d/Y',strtotime($reservation->ends_at)):''}}">
                             <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
                         </div>
                     </div>
@@ -66,12 +81,12 @@
                     <div class="form-group">
                         <label>Adults</label>
                         <select name="adult_count" class="form-control">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
+                            <option value="1" {{(isset($reservation) && $reservation->adult_count == 1)?'selected':''}}>1</option>
+                            <option value="2" {{(isset($reservation) && $reservation->adult_count == 2)?'selected':''}}>2</option>
+                            <option value="3" {{(isset($reservation) && $reservation->adult_count == 3)?'selected':''}}>3</option>
+                            <option value="4" {{(isset($reservation) && $reservation->adult_count == 4)?'selected':''}}>4</option>
+                            <option value="5" {{(isset($reservation) && $reservation->adult_count == 5)?'selected':''}}>5</option>
+                            <option value="6" {{(isset($reservation) && $reservation->adult_count == 6)?'selected':''}}>6</option>
                         </select>
                     </div>
                 </div>
@@ -79,13 +94,13 @@
                     <div class="form-group">
                         <label>Children <small>(Ages 6-15)</small></label>
                         <select name="children_count" class="form-control">
-                            <option value="0">0</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
+                            <option value="0" {{(isset($reservation) && $reservation->children_count == 0)?'selected':''}}>0</option>
+                            <option value="1" {{(isset($reservation) && $reservation->children_count == 1)?'selected':''}}>1</option>
+                            <option value="2" {{(isset($reservation) && $reservation->children_count == 2)?'selected':''}}>2</option>
+                            <option value="3" {{(isset($reservation) && $reservation->children_count == 3)?'selected':''}}>3</option>
+                            <option value="4" {{(isset($reservation) && $reservation->children_count == 4)?'selected':''}}>4</option>
+                            <option value="5" {{(isset($reservation) && $reservation->children_count == 5)?'selected':''}}>5</option>
+                            <option value="6" {{(isset($reservation) && $reservation->children_count == 6)?'selected':''}}>6</option>
                         </select>
                     </div>
                 </div>
@@ -94,13 +109,13 @@
                 <div class="col-sm-6">
                     <div class="control-group form-group {{(count($errors) > 0 && $errors->first('name'))?'has-error':''}}">
                         <label>Name</label>
-                        <input type="text" name="name" value="{{old('name')}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="Name">
+                        <input type="text" name="name" value="{{(isset($reservation))?$reservation->contact_name:''}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="Name">
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="control-group form-group {{(count($errors) > 0 && $errors->first('email'))?'has-error':''}}">
                         <label>Email Address</label>
-                        <input type="email" name="email" value="{{old('email')}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="Email">
+                        <input type="email" name="email" value="{{(isset($reservation))?$reservation->contact_email:''}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="Email">
                     </div>
                 </div>
             </div>
@@ -108,19 +123,19 @@
                 <div class="col-sm-6">
                     <div class="control-group form-group {{(count($errors) > 0 && $errors->first('phone'))?'has-error':''}}">
                         <label>Phone Number</label>
-                        <input type="tel" name="phone" value="{{old('phone')}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="Phone Number">
+                        <input type="tel" name="phone" value="{{(isset($reservation))?$reservation->contact_phone:''}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="Phone Number">
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="control-group form-group">
                         <label>Best Time to Call</label>
-                        <input type="text" name="best_time" value="{{old('best_time')}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="ex: 5:00pm">
+                        <input type="text" name="best_time" value="{{(isset($reservation))?$reservation->best_time:''}}" size="40" class="form-control" aria-required="true" aria-invalid="false" placeholder="ex: 5:00pm">
                     </div>
                 </div>
             </div>
             <div class="control-group form-group {{(count($errors) > 0 && $errors->first('comment'))?'has-error':''}}">
                 <label>User Comments</label>
-                <textarea name="comment" cols="40" rows="3" class="form-control" aria-required="true" aria-invalid="false">{{old('comment')}}</textarea>
+                <textarea name="comment" cols="40" rows="3" class="form-control" aria-required="true" aria-invalid="false">{{(isset($reservation))?$reservation->comment:''}}</textarea>
             </div>
         </form>
 
