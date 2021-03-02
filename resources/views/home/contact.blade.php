16f88b3cec219dfb985f72eefbffc02d08f5bb8b
Matt Crandell <mrcrandell@gmail.com>
2021-03-02 17:33:15 -0500

    Removed contact form.

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -8,17 +8,24 @@
 
 <h1>Contact Us</h1>
 
-<address>Rifle River Campground &amp; Canoe Livery<br>
-    5825 N Townline Rd<br>
-    PO Box 105<br>
-    Sterling, MI 48659</address>
-
-<p>Phone: <a href="tel:989-654-2556" target="_blank">989-654-2556</a><br>
-    Fax: <a href="tel:9896542521" target="_blank">989-654-2521</a><br>
-    Email: <a href="mailto:reservations@riflerivercampground.com" target="_blank">reservations@<wbr>riflerivercampground.com</a></p>
-<p><a href="https://www.facebook.com/RifleRiverCampground" target="_blank"><i class="fa fa-facebook-square"></i> Find us on Facebook</a></p>
-
 <div class="row">
+    <div class="col-md-6">
+        <address>Rifle River Campground &amp; Canoe Livery<br>
+            5825 N Townline Rd<br>
+            PO Box 105<br>
+            Sterling, MI 48659</address>
+
+        <p>Phone: <a href="tel:989-654-2556" target="_blank">989-654-2556</a><br>
+            Fax: <a href="tel:9896542521" target="_blank">989-654-2521</a><br>
+            <!--Email: <a href="mailto:reservations@riflerivercampground.com" target="_blank">reservations@<wbr>riflerivercampground.com</a></p>-->
+        <p><a href="https://www.facebook.com/RifleRiverCampground" target="_blank"><i class="fa fa-facebook-square"></i> Find us on Facebook</a></p>
+    </div>
+    <div class="col-md-6">
+        <div class="embed-responsive embed-responsive-4by3"><iframe class="embed-responsive-item" style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3898.1262963070376!2d-84.03930597052606!3d44.08462350490878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x586d7381e2b10c93!2sRifle+River+Campground+%26+Canoe+Livery!5e0!3m2!1sen!2sus!4v1498095161110" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
+    </div>
+</div>
+
+<!-- <div class="row">
     <div class="col-sm-6">
         @if (session('status'))
             <div class="alert alert-success">
@@ -65,8 +72,8 @@
         </form>
     </div>
     <div class="col-sm-6">
-        <div class="embed-responsive embed-responsive-4by3"><iframe class="embed-responsive-item" style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3898.1262963070376!2d-84.03930597052606!3d44.08462350490878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x586d7381e2b10c93!2sRifle+River+Campground+%26+Canoe+Livery!5e0!3m2!1sen!2sus!4v1498095161110" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>
+        
     </div>
-</div>
+</div> -->
 
 @stop
